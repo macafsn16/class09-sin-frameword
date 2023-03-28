463edfdf60e7257418afb656f5adc0a673105e01
macafsn16 <103944518+macafsn16@users.noreply.github.com>
2023-03-28 17:58:44 -0300

    actualizacion

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+import { createRoot } from '../react-dom/client';
+
 
 let myApp = document.getElementById("myApp");
 
 myApp.innerHTML = "Hello Words!!!";
 
 myApp.innerHTML = myApp.innerHTML +'<div id = "reactApp"></div>';
+
+const root = createRoot(document.getElementById(reactApp));
+root.render("<h1> Hello Words From React</h1>");
